3b523574462d38616aa80d91d4954f19562d335a
Chuma Okoro <cokoro@etsy.com>
2019-09-22 01:05:06 -0400

    move toast to top and fix error to show both toasts

--- app/src/main/java/com/example/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/geoquiz/QuizActivity.java
@@ -3,8 +3,10 @@ package com.example.geoquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -20,14 +22,18 @@ public class QuizActivity extends AppCompatActivity {
         this.mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Do nothing
+                Toast toast = Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 0);
+                toast.show();
             }
         });
-        this.mTrueButton = (Button) findViewById(R.id.true_button);
-        this.mTrueButton.setOnClickListener(new View.OnClickListener() {
+        this.mFalseButton = (Button) findViewById(R.id.false_button);
+        this.mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //DO nothing as well
+                Toast toast = Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 0);
+                toast.show();
             }
         });
     }
